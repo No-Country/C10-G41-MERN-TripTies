7613cb2ca4db9d338f5a3fcede7d7c758babeffb
Ghoul-JS <jmanuelgut170@gmail.com>
2023-03-27 17:44:10 -0500

    feat:Routes, install react-router-dom

--- client/src/components/LandingPage/LandingPage.tsx
+++ client/src/components/LandingPage/LandingPage.tsx
@@ -1,7 +1,13 @@
+import imgFly from "../../img/fly-paper.png"
+import style from "../../styles/LandingPage/LandingPage.module.css"
+
 const LandingPage = ():JSX.Element => {
     return (
         <>
-            
+            <div>
+                <img src={imgFly} alt="imgFly" />
+            </div>
+            <h1>TripTies</h1>
         </>
     )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import LandingPage from './components/LandingPage/LandingPage'
+import Home from './components/Home/Home';
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App/>
+  },
+  {
+    path:"/home",
+    element: <Home/>
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
