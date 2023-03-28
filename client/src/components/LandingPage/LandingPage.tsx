c52b2c475c2d1dfd85e02c074ee7ff24016a2ede
Ghoul-JS <jmanuelgut170@gmail.com>
2023-03-28 16:26:36 -0500

    feat: insert minifooter in landingPage

--- client/src/components/LandingPage/LandingPage.tsx
+++ client/src/components/LandingPage/LandingPage.tsx
@@ -2,6 +2,7 @@ import imgFly from "../../img/fly-paper.png"
 import imgGuy from"../../img/guy.png"
 import style from "../../styles/LandingPage/LandingPage.module.css"
 import NavBarL from "./NavBarL"
+import MiniFooter from "../MiniFooter/MiniFooter"
 
 const LandingPage = ():JSX.Element => {
     return (
@@ -19,9 +20,13 @@ const LandingPage = ():JSX.Element => {
                     </p>
                 </div>
                 <button className={style.btnExp}>Explore</button>
-            <div className={style.containerGuy}>
-             <img className={style.guyImg} src={imgGuy} alt="guy" />
-            </div>
+
+                <div className={style.containerGuy}>
+                    <img className={style.guyImg} src={imgGuy} alt="guy" />
+                    <div className={style.minFooter}>
+                        <MiniFooter />
+                    </div>
+                </div>
             </div>
         </>
     )
