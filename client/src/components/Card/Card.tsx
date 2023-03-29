2fd6659cc22aa9a2b930b5e3a288d4f8735b01bf
Julihumere <julihumereuner@gmail.com>
2023-03-29 20:41:03 -0300

    feat: add array with publications

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -1,69 +1,78 @@
-import React from 'react'
-import style from '../../styles/Card/Card.module.css';
-import avatar from '../../img/avatar.png'
-import menuVertical from '../../img/menu-vertical.png'
-import coffee from '../../img/coffee.png'
-import location from '../../img/smallPin.png'
-import londonCoffee from '../../img/coffeLondon.png'
-import boldHeart from '../../img/heart-circle-bold.png'
-import message from '../../img/message-text.png'
-import messageBig from '../../img/bigMessageText.png'
-import share from '../../img/send-2.png'
-import heart from '../../img/heart-circle.png'
-import save from '../../img/archive-tick.png'
-import stars from '../../img/stars.png'
+import React from "react";
+import style from "../../styles/Card/Card.module.css";
+import avatar from "../../img/avatar.png";
+import menuVertical from "../../img/menu-vertical.png";
+import coffee from "../../img/coffee.png";
+import location from "../../img/smallPin.png";
+import londonCoffee from "../../img/coffeLondon.png";
+import boldHeart from "../../img/heart-circle-bold.png";
+import message from "../../img/message-text.png";
+import messageBig from "../../img/bigMessageText.png";
+import share from "../../img/send-2.png";
+import heart from "../../img/heart-circle.png";
+import save from "../../img/archive-tick.png";
+import stars from "../../img/stars.png";
 
-
-function Card() {
+type props = {
+  places: any;
+};
+function Card({ places }: props) {
   return (
     <section className={style.container}>
-        <img src={avatar} alt="avatar"/>
-        <section className={style.content}>
+      <img src={avatar} alt="avatar" />
+      <section className={style.content}>
         <div className={style.userInfo}>
-            <aside>
-                <h4 className={style.name}>Jane Johnson</h4>
-                <span>2 hours</span>
-            </aside>
-            <img className={style.dotMenu} src={menuVertical} alt="dots menu"/>
+          <aside>
+            <h4 className={style.name}>{places.name}</h4>
+            <span>{places.time}</span>
+          </aside>
+          <img className={style.dotMenu} src={menuVertical} alt="dots menu" />
         </div>
         <article>
-            <p className={style.description}>In love with this cute little café I found in London. Great prices and delicious food!!! It's called Bartons and it's located in Bermondsey #London #CofeeShop</p>
+          <p className={style.description}>
+            {places.description} {places.tag}
+          </p>
         </article>
         <div className={style.publicationInfo}>
-            <aside>
-            <img src={stars} alt="stars"/>
-            </aside>
-            <aside>
-                <img src={coffee} alt="coffe"/>
-                <span>Food & beverage</span>
-            </aside>
-            <aside>
-                <img src={location} alt="location"/>
-                <span>London, UK</span>
-            </aside>
+          <aside>
+            <img src={places.stars} alt="stars" />
+          </aside>
+          <aside>
+            <img src={coffee} alt="coffe" />
+            <span>{places.attraction}</span>
+          </aside>
+          <aside>
+            <img src={location} alt="location" />
+            <span>{places.location}</span>
+          </aside>
         </div>
-        <img src={londonCoffee} alt="london coffee"/>
+        <img
+          src={places.photo}
+          alt="london coffee"
+          width="560px"
+          height="216px"
+        />
         <div className={style.likesAndComments}>
-            <aside>
-                <img src={boldHeart} alt="bold heart"/>
-                <span>32</span>
-            </aside>
-            <aside>
-                <img src={message} alt="message"/>
-                <span>4</span>
-            </aside>
+          <aside>
+            <img src={boldHeart} alt="bold heart" />
+            <span>{places.likes}</span>
+          </aside>
+          <aside>
+            <img src={message} alt="message" />
+            <span>{places.comments}</span>
+          </aside>
         </div>
         <div className={style.actions}>
-            <aside>
-                <img src={heart} alt="heart"/>
-                <img src={messageBig} alt="message"/>
-                <img src={share} alt="share"/>
-            </aside>
-            <img src={save} alt="save thick"/>
+          <aside>
+            <img src={heart} alt="heart" />
+            <img src={messageBig} alt="message" />
+            <img src={share} alt="share" />
+          </aside>
+          <img src={save} alt="save thick" />
         </div>
-        </section>
+      </section>
     </section>
-  )
+  );
 }
 
-export default Card
+export default Card;
