059b72d6de782e6d0d7ec672de24e2fa96aa53fa
francoarenas <franco.argenmu.35@gmail.com>
2023-03-30 18:49:06 -0300

    feat: section Chat scroll, button setting and button new message v2

--- client/src/components/SectionDiscover/SectionChat.tsx
+++ client/src/components/SectionDiscover/SectionChat.tsx
@@ -6,55 +6,69 @@ import avatar3 from '../../img/avatar3.png'
 import avatar4 from '../../img/avatar4.png'
 import avatar5 from '../../img/avatar5.png'
 import avatar6 from '../../img/avatar6.png'
+import add from '../../img/message-add.png'
+import setting from '../../img/setting.png'
 
 export default function SectionChat() {
     const users = [
         {
-            name : 'Sophia',
-            avatar : avatar1
+            name: 'Jane Johnson',
+            avatar: avatar1
         },
-        
+
         {
-            name : 'Emma',
-            avatar : avatar2
-        },
-        {
-            name : 'Jake',
-            avatar : avatar3
+            name: 'Jan Patrick',
+            avatar: avatar2
         },
         {
-            name : 'Olivia',
-            avatar : avatar4
+            name: 'Pablo Alvarado',
+            avatar: avatar3
         },
         {
-            name : 'Emily',
-            avatar : avatar5
+            name: 'Lea Bates',
+            avatar: avatar4
         },
         {
-            name : 'Connor',
-            avatar : avatar6
+            name: 'Flor Pérez',
+            avatar: avatar5
+        },
+        {
+            name: 'Thomas May',
+            avatar: avatar6
         }
     ]
-  return (
-    <>
-        <h3 className={styles.title}>Chat</h3>
-        <div className={styles.conteiner}>
-        <div className={styles.chats}>
-            <input type="text" placeholder='Search messages' id={styles['search']}/>
-
-            {
-                users.map(e => (
-                    <div className={styles.avatar}>
-                    <img src={e.avatar} alt={e.name} />
-                    <div className={styles.text}>
-                    <h2>{e.name}</h2>
-                    <p>Desc</p>
-                </div>
+    return (
+        <>
+            <h3 className={styles.title}>Chat</h3>
+            <div className={styles.conteiner}>
+                <div className={styles.chats}>
+                    <input type="text" placeholder='Search messages' id={styles['search']} />
+                    <div className={styles.scroll}>
+                        {
+                            users.map(e => (
+                                <div className={styles.avatar}>
+                                    <img src={e.avatar} alt={e.name} />
+                                    <div className={styles.text}>
+                                        <h2>{e.name}</h2>
+                                    </div>
+                                </div>
+                            ))
+                        }
                     </div>
-                ))
-            }
-        </div>
-        </div>
-    </>
-  )
+                    <div className={styles.config}>
+                        <hr />
+                        <div className={styles.messages}>
+                            <div className={styles.add}>
+                                <img src={add} alt="message add" />
+                                <button className={styles.buttons}>New message</button>
+                            </div>
+                            <div>
+                                <button className={styles.buttons}><img src={setting} alt="setting" /></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
