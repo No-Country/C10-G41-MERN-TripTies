9de62834536d38709165dc6f3a57848e2b644a29
Julihumere <julihumereuner@gmail.com>
2023-03-30 17:06:45 -0300

    feat: function button place i visited

--- client/src/components/LandingPage/LandingPage.tsx
+++ client/src/components/LandingPage/LandingPage.tsx
@@ -1,36 +1,40 @@
-import imgFly from "../../img/fly-paper.png"
-import imgGuy from"../../img/guy.png"
-import style from "../../styles/LandingPage/LandingPage.module.css"
-import NavBarL from "./NavBarL"
-import MiniFooter from "../MiniFooter/MiniFooter"
-import { Link } from "react-router-dom"
+import imgFly from "../../img/fly-paper.png";
+import imgGuy from "../../img/guy.png";
+import style from "../../styles/LandingPage/LandingPage.module.css";
+import NavBarL from "./NavBarL";
+import MiniFooter from "../MiniFooter/MiniFooter";
+import { Link } from "react-router-dom";
 
-const LandingPage = ():JSX.Element => {
-    return (
-        <>
-            <NavBarL />
-            <div className={style.containerLanding}> 
-                <div className={style.flyAndTitle}>
-                    <h1>TripTies</h1>
-                    <img src={imgFly} alt="imgFly" />
-                </div>
+const LandingPage = (): JSX.Element => {
+  return (
+    <>
+      <NavBarL />
+      <div className={style.containerLanding}>
+        <div className={style.flyAndTitle}>
+          <h1>TripTies</h1>
+          <img src={imgFly} alt="imgFly" />
+        </div>
 
-                <div className={style.txtAndBtn}>
-                    <p>
-                        Explore the world like never before. Connect with other wanderers just like you and discover a new way of traveling.
-                    </p>
-                </div>
-                <button className={style.btnExp}> <Link to="/home">Explore</Link> </button>
+        <div className={style.txtAndBtn}>
+          <p>
+            Explore the world like never before. Connect with other wanderers
+            just like you and discover a new way of traveling.
+          </p>
+        </div>
+        <button className={style.btnExp}>
+          {" "}
+          <Link to="/home">Explore</Link>
+        </button>
 
-                <div className={style.containerGuy}>
-                    <img className={style.guyImg} src={imgGuy} alt="guy" />
-                    <div className={style.minFooter}>
-                        <MiniFooter />
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
+        <div className={style.containerGuy}>
+          <img className={style.guyImg} src={imgGuy} alt="guy" />
+          <div className={style.minFooter}>
+            <MiniFooter />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default LandingPage;
