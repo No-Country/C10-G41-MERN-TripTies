015b781ffc522442d6260cb66c1be12b0b57da88
Lucas-Abba <abbalucas4@gmail.com>
2023-04-02 20:14:10 -0300

    feat: likes to comment

--- api/src/controllers/comment.controller.js
+++ api/src/controllers/comment.controller.js
@@ -1,16 +1,18 @@
 const { createComment, findComments, changeComment, removeComment } = require('../services/comment.services')
 
 const postComment = (req, res) => {
-  const { user_id, content } = req.body
-  createComment({ user_id, content })
+  const user_id = req.userToken._id
+  const { parent_id, content } = req.body
+  createComment({ user_id, parent_id, content })
     .then((response) => {
       res.status(201).json(response)
     })
     .catch((err) => {
       res.status(400).json({
         message: err.message, fields: {
-          user_id: 'String',
-          content: 'String'
+          parent_id : 'String-Optional',
+          content: 'String',
+          // publicationId: 'String'
         }
       })
     })
@@ -29,13 +31,16 @@ const getComments = (req, res) => {
 }
 
 const updateComment = (req, res) => {
-  changeComment(req.body)
+  const id = req.body.id
+  const user_id = req.userToken._id
+  changeComment(id, user_id)
     .then((response) => {
       res.status(200).json(response)
     })
     .catch((err) => {
       res.status(400).json({
         message: err.message, fields: {
+          id: 'String',
           user_id: 'String',
           reposted: 'Bool'
         }
