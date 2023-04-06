e5a111f4e3745b58d3f244e83021affe23657cbb
Julihumere <julihumereuner@gmail.com>
2023-04-06 14:04:50 -0300

    ...

--- api/src/database/database.js
+++ api/src/database/database.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
-const dbConection = mongoose.connection
+const mongoose = require("mongoose");
+require("dotenv").config();
+const dbConection = mongoose.connection;
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-const databaseConfig = require('./config')
+const databaseConfig = require("./config");
 
 mongoose
-  .connect(process.env.DATABASE_URI_DEV, databaseConfig[process.env.NODE_ENV])
-  .catch((error) => console.log(error))
-dbConection.on('open', (_) => {
-  console.log(`Database connected to Mongo Atlas at PORT ${PORT}`)
-})
-dbConection.on('error', (error) => {
-  console.log(error)
-})
+  .connect(process.env.DATABASE_URI_TEST, databaseConfig[process.env.NODE_ENV])
+  .catch((error) => console.log(error));
+dbConection.on("open", (_) => {
+  console.log(`Database connected to Mongo Atlas at PORT ${PORT}`);
+});
+dbConection.on("error", (error) => {
+  console.log(error);
+});
