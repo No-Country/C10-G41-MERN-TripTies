7b8cc391abc9112ee7e23ca441c08bbb708a7ed8
Lucas-Abba <abbalucas4@gmail.com>
2023-04-11 16:19:27 -0300

    fix: redundant if, use of cors

--- api/app.js
+++ api/app.js
@@ -25,15 +25,14 @@ const corsOptions = {
   }
 }
 
+app.use(cors())
+
 if (process.env.NODE_ENV === 'production') {
-  app.use(cors())
   /* Set security HTTP headers */
   /* For Error ERR_BLOCKED_BY_RESPONSE.NotSameOrigin 200 
        https://stackoverflow.com/questions/70752770/helmet-express-err-blocked-by-response-notsameorigin-200
   */
   app.use(helmet({ crossOriginResourcePolicy: false }))
-} else {
-  app.use(cors())
 }
 //! Accept Json & form-urlencoded
 app.use(express.json())
