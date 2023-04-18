c89e08520ea951f572b10ef399c6c52d5051d9e3
Julihumere <julihumereuner@gmail.com>
2023-04-18 17:56:13 -0300

    ...

--- api/src/controllers/tag.controller.js
+++ api/src/controllers/tag.controller.js
@@ -2,8 +2,6 @@ const Tag = require("../services/tag.services");
 
 const getAllTags = async (req, res, next) => {
   try {
-    // const page = parseInt(req.query.page);
-    // const limit = parseInt(req.query.limit);
     const { tags } = await Tag.findAllTags();
 
     res.status(200).json({
@@ -14,25 +12,4 @@ const getAllTags = async (req, res, next) => {
   }
 };
 
-const postTag = async (req, res, next) => {
-  // const info = req.body;
-  // console.log(info);
-  // Tag.createTag(info);
-  // .then((data) => {
-  //   res.status(200).json(data);
-  // })
-  // .catch((err) => {
-  //   res.status(400).json({
-  //     message: err.message,
-  //     fields: {
-  //       content: "String",
-  //       images: "req.files",
-  //       location: "point/coordinates",
-  //       reported: "Number",
-  //       rating: "Number",
-  //     },
-  //   });
-  // });
-};
-
-module.exports = { getAllTags, postTag };
+module.exports = { getAllTags };
--- api/src/services/profile.services.js
+++ api/src/services/profile.services.js
@@ -1,18 +1,17 @@
-const { default: mongoose } = require('mongoose')
-const Profile = require('../models/profiles.models')
-const User = require('../models/users.models')
-
+const { default: mongoose } = require("mongoose");
+const Profile = require("../models/profiles.models");
+const User = require("../models/users.models");
 
 const findAllUsersWithProfile = async ({ page = 1, limit = 10 }) => {
-  const skip = (page - 1) * limit
+  const skip = (page - 1) * limit;
 
   const usersWithProfile = await User.aggregate([
     {
       $lookup: {
-        from: 'profiles',
-        localField: '_id',
-        foreignField: 'user',
-        as: 'profile',
+        from: "profiles",
+        localField: "_id",
+        foreignField: "user",
+        as: "profile",
       },
     },
     {
@@ -21,47 +20,45 @@ const findAllUsersWithProfile = async ({ page = 1, limit = 10 }) => {
         username: 1,
         first_name: 1,
         last_name: 1,
-        photo: { $arrayElemAt: ['$profile.portrait', 0] },
-        description: { $arrayElemAt: ['$profile.description', 0] },
-        birthday: { $arrayElemAt: ['$profile.birthday', 0] },
+        photo: { $arrayElemAt: ["$profile.portrait", 0] },
+        description: { $arrayElemAt: ["$profile.description", 0] },
+        birthday: { $arrayElemAt: ["$profile.birthday", 0] },
       },
     },
-  ])
+  ]);
 
-  const count = usersWithProfile.length
-  const totalPages = Math.ceil(count / limit)
+  const count = usersWithProfile.length;
+  const totalPages = Math.ceil(count / limit);
 
-  const paginatedUsersWithProfile = usersWithProfile.slice(skip, skip + limit)
+  const paginatedUsersWithProfile = usersWithProfile.slice(skip, skip + limit);
 
-  return { usersWithProfile: paginatedUsersWithProfile, totalPages }
-}
-
+  return { usersWithProfile: paginatedUsersWithProfile, totalPages };
+};
 
 const findProfile = async (userId) => {
   try {
     // Buscamos el perfil del usuario por su ID y lo retornamos
-    const user = await User.findById(userId)
-    const profile = await Profile.findOne({ user: userId })
-    const userProfile = { profile, ...user }
-    return userProfile
+    const user = await User.findById(userId);
+    const profile = await Profile.findOne({ user: userId });
+    const userProfile = { profile, ...user };
+    return userProfile;
   } catch (error) {
-    throw Error('Not found Profile', 404, 'Not Found')
+    throw Error("Not found Profile", 404, "Not Found");
   }
-}
+};
 
 const editUserProfile = async (userId, userData) => {
-  const session = await mongoose.startSession()
-  session.startTransaction()
-  console.log(userId, userData)
+  const session = await mongoose.startSession();
+  session.startTransaction();
   try {
-    const user = await User.findById(userId).session(session)
-    const profile = await Profile.findOne({ user: userId }).session(session)
+    const user = await User.findById(userId).session(session);
+    const profile = await Profile.findOne({ user: userId }).session(session);
 
     if (!user) {
-      throw new Error('Not found user', 404, 'Not Found')
+      throw new Error("Not found user", 404, "Not Found");
     }
     if (!profile) {
-      throw new Error('Not found profiles', 404, 'Not Found')
+      throw new Error("Not found profiles", 404, "Not Found");
     }
     const updatedUser = await User.findByIdAndUpdate(
       userId,
@@ -73,7 +70,7 @@ const editUserProfile = async (userId, userData) => {
         },
       },
       { new: true, session }
-    )
+    );
     const updatedProfile = await Profile.findOneAndUpdate(
       { user: userId },
       {
@@ -84,19 +81,19 @@ const editUserProfile = async (userId, userData) => {
         },
       },
       { new: true, session }
-    )
-    await session.commitTransaction()
-    session.endSession()
-    return { updatedUser, updatedProfile }
+    );
+    await session.commitTransaction();
+    session.endSession();
+    return { updatedUser, updatedProfile };
   } catch (err) {
-    await session.abortTransaction()
-    session.endSession()
-    throw err
+    await session.abortTransaction();
+    session.endSession();
+    throw err;
   }
-}
+};
 
 module.exports = {
   findProfile,
   findAllUsersWithProfile,
   editUserProfile,
-}
+};
