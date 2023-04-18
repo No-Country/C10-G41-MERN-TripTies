e89bda913d9b580659f60edf128b15d66f440e3e
Julihumere <julihumereuner@gmail.com>
2023-04-18 18:14:31 -0300

    ...

--- api/src/controllers/user.controller.js
+++ api/src/controllers/user.controller.js
@@ -1,39 +1,42 @@
-const User = require('../services/user.services')
+const User = require("../services/user.services");
 
 const postUser = async (req, res) => {
   try {
     const { username, email, password, firstName, lastName, photo, role } =
-      req.body
-
-    const user = await User.createUser({
-      username,
-      firstName,
-      lastName,
-      email,
-      password,
-      photo,
-      role,
-    })
-    res.status(201).json(user)
+      req.body;
+    const userExist = await User.getUserByUsername(username);
+    if (!userExist) {
+      // const user = await User.createUser({
+      //   username,
+      //   firstName,
+      //   lastName,
+      //   email,
+      //   password,
+      //   photo,
+      //   role,
+      // })
+      res.status(201).json(user);
+    } else {
+    }
   } catch (error) {
     res.status(400).json({
       message: error.message,
       fields: {
-        username: 'String',
-        first_name: 'String',
-        last_name: 'String',
-        email: 'example@example.com',
-        password: 'String',
-        photo: 'URL',
+        username: "String",
+        first_name: "String",
+        last_name: "String",
+        email: "example@example.com",
+        password: "String",
+        photo: "URL",
       },
-    })
+    });
   }
-}
+};
 
 const postUserSocialNetwork = async (req, res) => {
   try {
     const { username, email, password, firstName, lastName, photo, role } =
-      req.body
+      req.body;
 
     const user = await User.createUser({
       username,
@@ -43,66 +46,65 @@ const postUserSocialNetwork = async (req, res) => {
       password,
       photo,
       role,
-    })
-    res.status(201).json(user)
+    });
+    res.status(201).json(user);
   } catch (error) {
     res.status(400).json({
       message: error.message,
       fields: {
-        username: 'String',
-        first_name: 'String',
-        last_name: 'String',
-        email: 'example@example.com',
-        password: 'String',
-        photo: 'URL',
+        username: "String",
+        first_name: "String",
+        last_name: "String",
+        email: "example@example.com",
+        password: "String",
+        photo: "URL",
       },
-    })
+    });
   }
-}
+};
 
 const getAllUsers = (req, res, next) => {
   User.getAllUsers()
     .then((users) => {
-      res.status(200).json(users)
+      res.status(200).json(users);
     })
     .catch((err) => {
-      next(err)
-    })
-}
+      next(err);
+    });
+};
 
 const getUserById = async (req, res, next) => {
-  const { userId } = req.params
+  const { userId } = req.params;
 
   try {
-    const user = await User.findUserById(userId, 'profile')
+    const user = await User.findUserById(userId, "profile");
     if (!user) {
-      throw new Error(404, 'User not found')
+      throw new Error(404, "User not found");
     }
-    res.status(200).json({ user })
+    res.status(200).json({ user });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 const deleteUser = (req, res) => {
-  const { userId } = req.params
+  const { userId } = req.params;
 
   User.removeUser(userId)
     .then((user) => {
-      res.status(200).json({ message: 'User deleted succesfully', user })
+      res.status(200).json({ message: "User deleted succesfully", user });
     })
     .catch((err) => {
-      res.status(400).json(err)
-    })
-}
+      res.status(400).json(err);
+    });
+};
 
 const getInfoUser = async (req, res) => {
-  let id = req.user._id
+  let id = req.user._id;
   await User.getUserInformation(id)
-    .then(data => res.status(200).json(data))
-    .catch(err => res.status(400).json({ message: err.message }))
-}
-
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(400).json({ message: err.message }));
+};
 
 module.exports = {
   postUser,
@@ -110,5 +112,5 @@ module.exports = {
   getAllUsers,
   getUserById,
   deleteUser,
-  getInfoUser
-}
+  getInfoUser,
+};
--- api/src/models/post.models.js
+++ api/src/models/post.models.js
@@ -1,32 +1,18 @@
-<<<<<<< HEAD
 const { Schema, model } = require("mongoose");
+const { default: mongoose } = require("mongoose");
 
 const postSchema = new Schema({
   user: { type: Object, required: true },
   content: { type: String, required: true },
   privacity: { type: String, enum: ["Public", "Private"], required: true },
-  photo: { type: [Object], required: true },
+  photo: [{ type: String }],
   video: { type: [Object], required: true },
   rate: { type: Number, min: 1, max: 5, required: true },
   name: { type: String, required: true },
   clasification: { type: String, required: true },
   tag: { type: [String], required: true },
-=======
-const { default: mongoose } = require('mongoose')
-const { Schema, model } = require('mongoose')
-
-const postSchema = new Schema({
-  user: { type: Object,  },
-  content: { type: String,  },
-  privacity: { type: String, enum: ['Public', 'Private'],   },
-  photo: [{ type: String }],
-  video: { type: [Object],  },
-  rate: { type: Number, min: 1, max: 5,  },
-  name: { type: String,  },
-  clasification: { type: String, },
->>>>>>> 240b48697cd802936863e4f399b8795f19229078
   reported: { type: Number, default: 0 },
-  liked: { type: mongoose.Schema.Types.ObjectId, ref: 'Like' },
+  liked: { type: mongoose.Schema.Types.ObjectId, ref: "Like" },
   comments: { type: Number, default: 0 },
 });
 
